54d73ba8d2c928fa0b91ac7033eecd6b3d5d2a92
cristan96dc <crscntn12@gmail.com>
2026-06-23 13:23:57 -0300

    Descripción de los cambios que hiciste

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,7 +10,7 @@ export default function Login({ onLogin }) {
 
     try {
       const response = await fetch(
-        "https://stockearce2026.pythonanywhere.com/pagos/api/pagos/api/verificar-superusuario/",
+        "https://stockearce2026.pythonanywhere.com/pagos/api/verificar-superusuario/",
         {
           method: "POST",
           headers: {
